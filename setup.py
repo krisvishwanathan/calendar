7727a72fd20f9c9aa3226dad1f292ea02e3cc2db
Kris Vishwanathan <kris@icloudnow.com>
2024-05-29 22:04:09 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='calendar',
+    name='stcalander',
     version='1.0',
     packages=find_packages(),
+    include_package_data=True,
     include_package_data=True,
         install_requires=[
         "django>=4.0",
